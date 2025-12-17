674d897d04757f67af308338ebb8ece71b52a132
Otibaco <orijifranklin42@gmail.com>
2025-12-17 12:15:50 +0100

    fixing code8

--- backend/notification-service/src/main/java/com/example/notification_service/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/example/notification_service/NotificationServiceApplication.java
@@ -9,5 +9,6 @@ public class NotificationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
+	// IGNORE ---
 
 }
